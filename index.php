db7a7fc56fa8c46aa1098218893a0c5b3b0b10b1
DorKatzir <DorKatzir@users.noreply.github.com>
2021-08-22 10:08:58 +0300

    test

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
-<?php 
-echo 'Hello Dor';
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Primer - Index</title>
+</head>
+<body>
+    <h1>Hello 🐪 </h1>
+</body>
+</html>
